6cb81623e5bb27e096b06ad7591040bbaa2ebd07
bstriker <strikeforcezero@gmail.com>
2023-10-12 15:59:38 -0400

    refactor(utils): replace dynamic name with static name to survive minification

--- src/utils/errors/AssertionError.ts
+++ src/utils/errors/AssertionError.ts
@@ -1,10 +1,11 @@
 import { removeErrorConstructorFromStackTrace } from '../error';
 
+export const Identifier = 'AssertionError';
+
 export class AssertionError extends Error {
   constructor(message?: string) {
     super(message);
-    // Set the name of the error to the ClassName.
-    this.name = this.constructor.name;
+    this.name = Identifier;
 
     removeErrorConstructorFromStackTrace(this);
   }
--- src/utils/errors/AssertionValueTypeMismatchError.ts
+++ src/utils/errors/AssertionValueTypeMismatchError.ts
@@ -2,11 +2,13 @@ import { getClassName } from '../object';
 import { stringifySafe } from '../stringifySafe';
 import { AssertionError } from './AssertionError';
 
+
+export const Identifier = 'AssertionValueTypeMismatchError';
+
 export class AssertionValueTypeMismatchError extends AssertionError {
   constructor(value: unknown, expectedType: string) {
     super(AssertionValueTypeMismatchError.createMessage(value, expectedType));
-    // Set the name of the error to the ClassName.
-    this.name = this.constructor.name;
+    this.name = Identifier;
   }
 
   public static createMessage(value: unknown, expectedType: string): string {
