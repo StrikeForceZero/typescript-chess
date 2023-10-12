2214dddb93288502773b818383dcaeaa8eb8e31c
bstriker <strikeforcezero@gmail.com>
2023-10-12 17:21:59 -0400

    feat(utils): expand char utils to have assertion, and opaque type

--- src/utils/char.ts
+++ src/utils/char.ts
@@ -1,3 +1,14 @@
-export function isChar(input: string): boolean {
-  return input.length === 1;
+import { Opaque } from 'ts-essentials';
+import { AssertionValueTypeMismatchError } from './errors/AssertionValueTypeMismatchError';
+
+export type Char = Opaque<string, 'char'>
+
+export function isChar(value: string): value is Char {
+  return value.length === 1;
+}
+
+export function assertIsChar(value: string): asserts value is Char {
+  if (!isChar(value)) {
+    throw new AssertionValueTypeMismatchError(value, 'Char');
+  }
 }
