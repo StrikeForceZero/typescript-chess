801548a47966ac8abd636af35b64563020c1551e
bstriker <strikeforcezero@gmail.com>
2023-10-12 01:13:23 -0400

    chore(formatting): Adjust formatting in BoardFile

--- src/lib/board/BoardFile.ts
+++ src/lib/board/BoardFile.ts
@@ -1,3 +1,5 @@
+import { isChar } from '../utils/StringUtils';
+
 export enum BoardFile {
   A = 'a',
   B = 'b',
@@ -11,44 +13,44 @@ export enum BoardFile {
 
 export function toNumber(file: BoardFile): number {
   switch (file) {
-    case BoardFile.A:
-      return 0;
-    case BoardFile.B:
-      return 1;
-    case BoardFile.C:
-      return 2;
-    case BoardFile.D:
-      return 3;
-    case BoardFile.E:
-      return 4;
-    case BoardFile.F:
-      return 5;
-    case BoardFile.G:
-      return 6;
-    case BoardFile.H:
-      return 7;
+    case BoardFile.A: return 0;
+    case BoardFile.B: return 1;
+    case BoardFile.C: return 2;
+    case BoardFile.D: return 3;
+    case BoardFile.E: return 4;
+    case BoardFile.F: return 5;
+    case BoardFile.G: return 6;
+    case BoardFile.H: return 7;
   }
 }
 
 export function fromNumber(value: number): BoardFile {
   switch (value) {
-    case 0:
-      return BoardFile.A;
-    case 1:
-      return BoardFile.B;
-    case 2:
-      return BoardFile.C;
-    case 3:
-      return BoardFile.D;
-    case 4:
-      return BoardFile.E;
-    case 5:
-      return BoardFile.F;
-    case 6:
-      return BoardFile.G;
-    case 7:
-      return BoardFile.H;
-    default:
-      throw new Error(`Invalid value: ${value}`);
+    case 0: return BoardFile.A;
+    case 1: return BoardFile.B;
+    case 2: return BoardFile.C;
+    case 3: return BoardFile.D;
+    case 4: return BoardFile.E;
+    case 5: return BoardFile.F;
+    case 6: return BoardFile.G;
+    case 7: return BoardFile.H;
+    default: throw new Error(`Invalid value: ${value}`);
+  }
+}
+
+export function fromChar(char: string): BoardFile {
+  if (!isChar(char)) {
+    throw new Error(`'${char}' is not a char`);
+  }
+  switch (char.toLowerCase()) {
+    case 'a': return BoardFile.A;
+    case 'b': return BoardFile.B;
+    case 'c': return BoardFile.C;
+    case 'd': return BoardFile.D;
+    case 'e': return BoardFile.E;
+    case 'f': return BoardFile.F;
+    case 'g': return BoardFile.G;
+    case 'h': return BoardFile.H;
+    default: throw new Error(`Invalid value: ${char}`)
   }
 }
