99ef4838e150b605e174c7df64bacfffcae7a59f
bstriker <strikeforcezero@gmail.com>
2023-10-12 02:53:14 -0400

    feat(pieces): add constants for all standard chess pieces

--- src/lib/piece/ChessPiece.ts
+++ src/lib/piece/ChessPiece.ts
@@ -1,4 +1,9 @@
-import { ColoredPiece } from './ColoredPiece';
+import {
+  ColoredPiece,
+  from as coloredPieceFrom,
+} from './ColoredPiece';
+import { PieceColor } from './PieceColor';
+import { PieceType } from './PieceType';
 
 export enum Kind {
   None,
@@ -25,3 +30,19 @@ export function from(piece: ColoredPiece): ColoredPieceContainer {
     piece,
   };
 }
+
+
+export const WhitePawn = from(coloredPieceFrom(PieceColor.White, PieceType.Pawn));
+export const WhiteKnight = from(coloredPieceFrom(PieceColor.White, PieceType.Knight));
+export const WhiteBishop = from(coloredPieceFrom(PieceColor.White, PieceType.Bishop));
+export const WhiteRook = from(coloredPieceFrom(PieceColor.White, PieceType.Rook));
+export const WhiteQueen = from(coloredPieceFrom(PieceColor.White, PieceType.Queen));
+export const WhiteKing = from(coloredPieceFrom(PieceColor.White, PieceType.King));
+
+export const BlackPawn = from(coloredPieceFrom(PieceColor.Black, PieceType.Pawn));
+export const BlackKnight = from(coloredPieceFrom(PieceColor.Black, PieceType.Knight));
+export const BlackBishop = from(coloredPieceFrom(PieceColor.Black, PieceType.Bishop));
+export const BlackRook = from(coloredPieceFrom(PieceColor.Black, PieceType.Rook));
+export const BlackQueen = from(coloredPieceFrom(PieceColor.Black, PieceType.Queen));
+export const BlackKing = from(coloredPieceFrom(PieceColor.Black, PieceType.King));
+
