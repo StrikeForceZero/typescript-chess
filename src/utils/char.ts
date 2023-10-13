0f19c73d15b500df8ae895b6d4a7050f3874040c
bstriker <strikeforcezero@gmail.com>
2023-10-13 15:30:42 -0400

    feat(utils): update Char to preserve underlying constant value

--- src/utils/char.ts
+++ src/utils/char.ts
@@ -1,18 +1,18 @@
 import { Opaque } from 'ts-essentials';
 import { AssertionValueTypeMismatchError } from './errors/AssertionValueTypeMismatchError';
 
-export type Char = Opaque<string, 'char'>
+export type Char<TChar extends string = string> = Opaque<TChar, 'char'>
 
-export function Char(input: string): Char {
+export function Char<TChar extends string>(input: TChar): Char<TChar> {
   assertIsChar(input);
   return input;
 }
 
-export function isChar(value: string): value is Char {
+export function isChar<TChar extends string>(value: TChar): value is Char<TChar> {
   return value.length === 1;
 }
 
-export function assertIsChar(value: string): asserts value is Char {
+export function assertIsChar<TChar extends string>(value: TChar): asserts value is Char<TChar> {
   if (!isChar(value)) {
     throw new AssertionValueTypeMismatchError(value, 'Char');
   }
