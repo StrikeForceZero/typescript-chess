3a16450db324f55cbb306076da4f690396827a9f
bstriker <strikeforcezero@gmail.com>
2023-10-13 15:39:51 -0400

    feat(board/utils): add next/prev utilities for BoardRank and BoardFile

--- src/board/utils/BoardFileUtils.ts
+++ src/board/utils/BoardFileUtils.ts
@@ -1,6 +1,43 @@
 import { assertExhaustive } from '../../utils/assert';
 import { BoardFile } from '../BoardFile';
 
+export function next(boardRank: BoardFile, wrapAround: boolean = false): BoardFile | null {
+  return nextBoardFile(boardRank, wrapAround);
+}
+
+export function prev(boardRank: BoardFile, wrapAround: boolean = false): BoardFile | null {
+  return prevBoardFile(boardRank, wrapAround);
+}
+
+export function nextBoardFile(boardFile: BoardFile, wrapAround: boolean = false): BoardFile | null {
+  switch (boardFile) {
+    case BoardFile.A: return BoardFile.B;
+    case BoardFile.B: return BoardFile.C;
+    case BoardFile.C: return BoardFile.D;
+    case BoardFile.D: return BoardFile.E;
+    case BoardFile.E: return BoardFile.F;
+    case BoardFile.F: return BoardFile.G;
+    case BoardFile.G: return BoardFile.H;
+    case BoardFile.H: return wrapAround ? BoardFile.A : null;
+    default: return assertExhaustive(boardFile, 'BoardFile');
+  }
+}
+
+export function prevBoardFile(boardFile: BoardFile, wrapAround: boolean = false): BoardFile | null {
+  switch (boardFile) {
+    case BoardFile.H: return BoardFile.G;
+    case BoardFile.G: return BoardFile.F;
+    case BoardFile.F: return BoardFile.E;
+    case BoardFile.E: return BoardFile.D;
+    case BoardFile.D: return BoardFile.C;
+    case BoardFile.C: return BoardFile.B;
+    case BoardFile.B: return BoardFile.A;
+    case BoardFile.A: return wrapAround ? BoardFile.H : null;
+    default: return assertExhaustive(boardFile, 'BoardFile');
+  }
+}
+
+
 export function* boardFileGenerator(start: BoardFile = BoardFile.A, wrapAround: boolean = false): Generator<BoardFile> {
   let currentFile = start;
 
--- src/board/utils/BoardRankUtils.ts
+++ src/board/utils/BoardRankUtils.ts
@@ -1,4 +1,41 @@
+import { assertExhaustive } from '../../utils/assert';
 import { BoardRank } from '../BoardRank';
+
+export function next(boardRank: BoardRank, wrapAround: boolean = false): BoardRank | null {
+  return nextBoardRank(boardRank, wrapAround);
+}
+
+export function prev(boardRank: BoardRank, wrapAround: boolean = false): BoardRank | null {
+  return prevBoardRank(boardRank, wrapAround);
+}
+
+export function nextBoardRank(boardRank: BoardRank, wrapAround: boolean = false): BoardRank | null {
+  switch (boardRank) {
+    case BoardRank.ONE: return BoardRank.TWO;
+    case BoardRank.TWO: return BoardRank.THREE;
+    case BoardRank.THREE: return BoardRank.FOUR;
+    case BoardRank.FOUR: return BoardRank.FIVE;
+    case BoardRank.FIVE: return BoardRank.SIX;
+    case BoardRank.SIX: return BoardRank.SEVEN;
+    case BoardRank.SEVEN: return BoardRank.EIGHT;
+    case BoardRank.EIGHT: return wrapAround ? BoardRank.ONE : null;
+    default: return assertExhaustive(boardRank, 'BoardRank');
+  }
+}
+
+export function prevBoardRank(boardRank: BoardRank, wrapAround: boolean = false): BoardRank | null {
+  switch (boardRank) {
+    case BoardRank.EIGHT: return BoardRank.SEVEN;
+    case BoardRank.SEVEN: return BoardRank.SIX;
+    case BoardRank.SIX: return BoardRank.FIVE;
+    case BoardRank.FIVE: return BoardRank.FOUR;
+    case BoardRank.FOUR: return BoardRank.THREE;
+    case BoardRank.THREE: return BoardRank.TWO;
+    case BoardRank.TWO: return BoardRank.ONE;
+    case BoardRank.ONE: return wrapAround ? BoardRank.EIGHT : null;
+    default: return assertExhaustive(boardRank, 'BoardRank');
+  }
+}
 
 export function* boardRankGenerator(start: BoardRank = BoardRank.ONE, wrapAround: boolean = false): Generator<BoardRank> {
   let currentRank = start;
