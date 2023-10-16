97fb24ccb78e865f5f10f9708e8afaa134dd4c21
bstriker <strikeforcezero@gmail.com>
2023-10-16 18:18:56 -0400

    refactor(utils): deepFreeze simple enums

--- src/utils/SimpleEnum.ts
+++ src/utils/SimpleEnum.ts
@@ -1,4 +1,8 @@
-import { ValueOf } from 'type-fest';
+import {
+  ReadonlyDeep,
+  ValueOf,
+} from 'type-fest';
+import { deepFreeze } from './deepFreeze';
 
 export type SimpleEnum<T extends Record<K, V>, K extends PropertyKey, V> = T;
 export type SimpleEnumTypeOf<T> = ValueOf<T>;
@@ -21,8 +25,8 @@ export type SimpleEnumTypeOf<T> = ValueOf<T>;
  * @param {T} obj - The object to be returned as an enum.
  * @returns {T} - The input object returned as is.
  */
-export function asSimpleEnum<T extends Record<K, V>, K extends PropertyKey, V>(obj: T): SimpleEnum<T, K, V> {
-  return obj;
+export function asSimpleEnum<T extends Record<K, V>, K extends PropertyKey, V>(obj: T): ReadonlyDeep<SimpleEnum<T, K, V>> {
+  return deepFreeze(obj);
 }
 
 /**
@@ -44,8 +48,8 @@ export function asSimpleEnum<T extends Record<K, V>, K extends PropertyKey, V>(o
  * @param {T} obj - The object to be returned as an enum.
  * @returns {T} - The input object returned as is, with the `const` modifier applied to the type parameter V.
  */
-export function asSimpleConstEnum<const T extends Record<K, V>, const K extends PropertyKey, const V>(obj: T): SimpleEnum<T, K, V> {
-  return obj;
+export function asSimpleConstEnum<const T extends Record<K, V>, const K extends PropertyKey, const V>(obj: T): ReadonlyDeep<SimpleEnum<T, K, V>> {
+  return deepFreeze(obj);
 }
 
 
