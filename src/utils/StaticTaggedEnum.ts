f646fd9f58b485289978dfc62684c95c38ba630e
bstriker <strikeforcezero@gmail.com>
2023-10-16 18:24:31 -0400

    refactor(utils): change TaggedEnum to StaticTaggedEnum

--- src/utils/StaticTaggedEnum.ts
+++ src/utils/StaticTaggedEnum.ts
@@ -5,14 +5,14 @@ import {
 import { deepFreeze } from './deepFreeze';
 
 type Variant<T, U> = U & { kind: T };
-type TaggedEnum<T> = {
+type StaticTaggedEnum<T> = {
   [K in keyof T]: Variant<K, T[K]>;
 };
 
-export type TaggedEnumTypeOf<T> = ValueOf<T>;
+export type StaticTaggedEnumTypeOf<T> = ValueOf<T>;
 
 /**
- * Creates a tagged enum from a provided definition object.
+ * Creates a static tagged enum from a provided definition object.
  *
  * A tagged enum is an object where each property is a distinct variant
  * of the enum. Each variant has a `kind` property which acts as a tag
@@ -23,11 +23,11 @@ export type TaggedEnumTypeOf<T> = ValueOf<T>;
  *
  * ## Example:
  * ```typescript
- * const Thing = createTaggedEnum({
+ * const Thing = createStaticTaggedEnum({
  *   Something: { something: 1 },
  *   OtherThing: { somethingElse: 2 },
  * });
- * type Thing = TaggedEnumTypeOf<typeof Thing>;
+ * type Thing = StaticTaggedEnumTypeOf<typeof Thing>;
  *
  * // Using the enum in a switch-case:
  * function returnValue(thing: Thing): number {
@@ -49,18 +49,18 @@ export type TaggedEnumTypeOf<T> = ValueOf<T>;
  *       return thing.something;
  *     case Thing.OtherThing.kind:
  *       return thing.somethingElse;
- *     default: throw new Error(`unknown thing ${thing.kind}`);
+ *     default: throw new Error(`unknown thing ${thing}`);
  *   }
  * }
  * ```
  *
  * @template T - The type of the definition object, extending a record of string keys
  *               to records of string keys to unknown values.
- * @param {T} defs - The definition object based on which the tagged enum is created.
- * @returns {TaggedEnum<T>} - The created tagged enum with variants.
+ * @param {T} defs - The definition object based on which the static tagged enum is created.
+ * @returns {StaticTaggedEnum<T>} - The created static tagged enum with variants.
  */
-export function createTaggedEnum<const T extends Record<string, ReadonlyDeep<Record<string, unknown>>>>(defs: T): ReadonlyDeep<TaggedEnum<T>> {
-  const result: Partial<TaggedEnum<T>> = {};
+export function createStaticTaggedEnum<const T extends Record<string, ReadonlyDeep<Record<string, unknown>>>>(defs: T): ReadonlyDeep<StaticTaggedEnum<T>> {
+  const result: Partial<StaticTaggedEnum<T>> = {};
 
   for (const key of Object.keys(defs) as (keyof T)[]) {
     result[key] = (
@@ -71,6 +71,5 @@ export function createTaggedEnum<const T extends Record<string, ReadonlyDeep<Rec
     );
   }
 
-  return deepFreeze(result as TaggedEnum<T>);
+  return deepFreeze(result as StaticTaggedEnum<T>);
 }
-
