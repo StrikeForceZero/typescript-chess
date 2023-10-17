57138bafc583c00d76e63fd7bc9e499616e8bf23
bstriker <strikeforcezero@gmail.com>
2023-10-16 23:32:24 -0400

    feat(utils): add entries to object utils

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,6 +1,12 @@
+import { ValueOf } from 'type-fest';
+
 export function getClassName(value: unknown): string | null {
   if (value && typeof value === 'object' && value.constructor && value.constructor.name) {
     return value.constructor.name;
   }
   return null;
+}
+
+export function entries<T extends Record<string, unknown>>(obj: T): Iterable<[keyof T, ValueOf<T>]> {
+  return Object.entries(obj) as Iterable<[keyof T, ValueOf<T>]>;
 }
