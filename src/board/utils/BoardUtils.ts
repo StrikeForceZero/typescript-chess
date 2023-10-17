54bc4f676a891d6088640d21beaa3cd174234cfb
bstriker <strikeforcezero@gmail.com>
2023-10-17 15:39:19 -0400

    feat(board/utils): add helper board helper methods isAtStartingPos, and isPieceAtStartingPos

--- src/board/utils/BoardUtils.ts
+++ src/board/utils/BoardUtils.ts
@@ -1,7 +1,11 @@
+import { deserializeBoardOnlyFENString } from '../../fen/deserializer';
+import { StandardStartPositionBoardOnlyFEN } from '../../fen/FENString';
+import { serializeBoardOnlyFENString } from '../../fen/serialize';
 import {
   ChessPieceColored,
   isColoredPieceContainer,
 } from '../../piece/ChessPiece';
+import { deepFreeze } from '../../utils/deepFreeze';
 import { Board } from '../Board';
 import { BoardPosition } from '../BoardPosition';
 
@@ -11,4 +15,14 @@ export function getChessPieceColoredOrThrow(board: Board, pos: BoardPosition): C
     throw new Error(`no piece found at ${pos}`);
   }
   return piece;
+}
+
+export function isBoardAtStartingPos(board: Board): boolean {
+  return serializeBoardOnlyFENString(board) === StandardStartPositionBoardOnlyFEN;
+}
+
+const startingPosBoard = deepFreeze(deserializeBoardOnlyFENString(StandardStartPositionBoardOnlyFEN));
+// will return true for comparing empty squares A3-F8
+export function isPieceAtStartingPos(board: Board, pos: BoardPosition): boolean {
+  return startingPosBoard.getPieceFromPos(pos) === board.getPieceFromPos(pos);
 }
