b28f93c36ec0cada63dbc66ff0b38fc291b9c679
bstriker <strikeforcezero@gmail.com>
2023-10-17 17:04:50 -0400

    refactor(move): make move update game state castling rights after each move

--- src/move/move.ts
+++ src/move/move.ts
@@ -2,10 +2,12 @@ import { BoardPosition } from '../board/BoardPosition';
 import { getChessPieceColoredOrThrow } from '../board/utils/BoardUtils';
 import { ChessPiece } from '../piece/ChessPiece';
 import { GameState } from '../state/GameState';
+import { updateCastleRights } from '../state/utils/CastlingRightsUtils';
 
 export function move(gameState: GameState, from: BoardPosition, to: BoardPosition): ChessPiece {
   const movingPiece = getChessPieceColoredOrThrow(gameState.board, from);
   const capturePiece = gameState.board.removePieceFromPos(to);
   gameState.board.placePieceFromPos(movingPiece, to);
+  updateCastleRights(gameState.board, gameState.castlingRights);
   return capturePiece;
 }
