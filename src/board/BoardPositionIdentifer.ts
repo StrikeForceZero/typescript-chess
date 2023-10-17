4594af741967f9fcedc475db8900f88e947bccbc
bstriker <strikeforcezero@gmail.com>
2023-10-17 17:03:46 -0400

    feat(board): add BoardPositionIdentifiers

---
+++ src/board/BoardPositionIdentifer.ts
@@ -0,0 +1,38 @@
+import { BoardFile } from './BoardFile';
+import { BoardRank } from './BoardRank';
+
+// White pieces
+export const SquareWhiteQueenSideRook = [BoardFile.A, BoardRank.ONE] as const;
+export const SquareWhiteKingSideRook = [BoardFile.H, BoardRank.ONE] as const;
+export const SquareWhiteQueenSideKnight = [BoardFile.B, BoardRank.ONE] as const;
+export const SquareWhiteKingSideKnight = [BoardFile.G, BoardRank.ONE] as const;
+export const SquareWhiteQueenSideBishop = [BoardFile.C, BoardRank.ONE] as const;
+export const SquareWhiteKingSideBishop = [BoardFile.F, BoardRank.ONE] as const;
+export const SquareWhiteQueen = [BoardFile.D, BoardRank.ONE] as const;
+export const SquareWhiteKing = [BoardFile.E, BoardRank.ONE] as const;
+export const SquareWhitePawnA = [BoardFile.A, BoardRank.TWO] as const;
+export const SquareWhitePawnB = [BoardFile.B, BoardRank.TWO] as const;
+export const SquareWhitePawnC = [BoardFile.C, BoardRank.TWO] as const;
+export const SquareWhitePawnD = [BoardFile.D, BoardRank.TWO] as const;
+export const SquareWhitePawnE = [BoardFile.E, BoardRank.TWO] as const;
+export const SquareWhitePawnF = [BoardFile.F, BoardRank.TWO] as const;
+export const SquareWhitePawnG = [BoardFile.G, BoardRank.TWO] as const;
+export const SquareWhitePawnH = [BoardFile.H, BoardRank.TWO] as const;
+
+// Black pieces
+export const SquareBlackQueenSideRook = [BoardFile.A, BoardRank.EIGHT] as const;
+export const SquareBlackKingSideRook = [BoardFile.H, BoardRank.EIGHT] as const;
+export const SquareBlackQueenSideKnight = [BoardFile.B, BoardRank.EIGHT] as const;
+export const SquareBlackKingSideKnight = [BoardFile.G, BoardRank.EIGHT] as const;
+export const SquareBlackQueenSideBishop = [BoardFile.C, BoardRank.EIGHT] as const;
+export const SquareBlackKingSideBishop = [BoardFile.F, BoardRank.EIGHT] as const;
+export const SquareBlackQueen = [BoardFile.D, BoardRank.EIGHT] as const;
+export const SquareBlackKing = [BoardFile.E, BoardRank.EIGHT] as const;
+export const SquareBlackPawnA = [BoardFile.A, BoardRank.SEVEN] as const;
+export const SquareBlackPawnB = [BoardFile.B, BoardRank.SEVEN] as const;
+export const SquareBlackPawnC = [BoardFile.C, BoardRank.SEVEN] as const;
+export const SquareBlackPawnD = [BoardFile.D, BoardRank.SEVEN] as const;
+export const SquareBlackPawnE = [BoardFile.E, BoardRank.SEVEN] as const;
+export const SquareBlackPawnF = [BoardFile.F, BoardRank.SEVEN] as const;
+export const SquareBlackPawnG = [BoardFile.G, BoardRank.SEVEN] as const;
+export const SquareBlackPawnH = [BoardFile.H, BoardRank.SEVEN] as const;
