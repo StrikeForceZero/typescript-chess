faf11b2343d7af6ef6ac1c065886f1b7f424c704
bstriker <strikeforcezero@gmail.com>
2023-10-17 20:23:32 -0400

    feat(state): add get/set methods utilizing CastleSide enum for CastlingRightsForColor

--- src/state/CastlingRightsForColor.ts
+++ src/state/CastlingRightsForColor.ts
@@ -1,4 +1,21 @@
+import { CastleSide } from '../board/CastleSide';
+import { assertExhaustive } from '../utils/assert';
+
 export class CastlingRightsForColor {
   public kingSide: boolean = true;
   public queenSide: boolean = true;
+  public get(side: CastleSide): boolean {
+    switch (side) {
+      case CastleSide.QueenSide: return this.queenSide;
+      case CastleSide.KingSide: return this.kingSide;
+      default: return assertExhaustive(side);
+    }
+  }
+  public set(side: CastleSide, state: boolean): void {
+    switch (side) {
+      case CastleSide.QueenSide: this.queenSide = state; break;
+      case CastleSide.KingSide: this.kingSide = state; break;
+      default: assertExhaustive(side);
+    }
+  }
 }
