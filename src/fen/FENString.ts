8b592f4816e67183fbbf3fd2c127655fb27e13e8
bstriker <strikeforcezero@gmail.com>
2023-10-18 09:43:21 -0400

    feat(fen): add method for parsing a fen string into parts

--- src/fen/FENString.ts
+++ src/fen/FENString.ts
@@ -23,6 +23,52 @@ export function isFen(input: string): input is FENString {
   return true;
 }
 
+export function getParts(
+  fenString: FENString,
+): readonly [
+  boardString: string,
+  activeColorString: string,
+  castleRightsString: string,
+  enPassantTargetSquareString: string,
+  halfMoveClock: number,
+  fullMoveClock: number,
+] {
+  const parts = fenString.split(' ');
+
+  if (parts.length !== 6) {
+    throw new Error('invalid FEN');
+  }
+
+  const [
+    boardString,
+    activeColorString,
+    castleRightsString,
+    enPassantTargetSquareString,
+    halfMoveClockString,
+    fullMoveClockString,
+  ] = parts;
+
+  if (
+    !boardString
+    || !activeColorString
+    || !castleRightsString
+    || !enPassantTargetSquareString
+    || !halfMoveClockString
+    || !fullMoveClockString
+  ) {
+    throw new Error('Invalid FEN');
+  }
+
+  return [
+    boardString,
+    activeColorString,
+    castleRightsString,
+    enPassantTargetSquareString,
+    Number(halfMoveClockString),
+    Number(fullMoveClockString),
+  ] as const;
+}
+
 export type FENStringBoardOnly = Tagged<string, 'FEN_BOARD'>;
 
 export const EmptyBoardOnlyFEN: FENStringBoardOnly = '8/8/8/8/8/8/8/8' as FENStringBoardOnly;
