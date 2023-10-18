32cc0bbf6e7494656fd9424c1b3c4a5099952cad
bstriker <strikeforcezero@gmail.com>
2023-10-17 23:33:12 -0400

    feat(state/utils): add CheckUtils

---
+++ src/state/utils/CheckUtils.ts
@@ -0,0 +1,47 @@
+import { deserialize } from '../../fen/deserializer';
+import { serialize } from '../../fen/serialize';
+import { PieceMoveMap } from '../../move/PieceMoveMap';
+import { ChessPiece } from '../../piece/ChessPiece';
+import { PieceType } from '../../piece/PieceType';
+import { GameState } from '../GameState';
+
+export function isCheck(gameState: GameState, activeColor = gameState.activeColor): boolean {
+  for (const square of gameState.board) {
+    if (!ChessPiece.ColoredPiece.is(square.piece)) continue;
+    // skip current player pieces
+    if (square.piece.coloredPiece.color === activeColor) continue;
+    const moves = PieceMoveMap[square.piece.coloredPiece.pieceType](square.piece.coloredPiece.color);
+    for (const move of moves) {
+      const validMoves = move.test(gameState, square.pos);
+      for (const validMove of validMoves) {
+        if (!validMove.expectedCapturePos) continue;
+        const targetPiece = gameState.board.getPieceFromPos(validMove.expectedCapturePos);
+        if (!ChessPiece.ColoredPiece.is(targetPiece)) continue;
+        if (targetPiece.coloredPiece.pieceType === PieceType.King) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+export function isCheckMate(gameState: GameState): boolean {
+  if (!isCheck(gameState)) return false;
+  for (const square of gameState.board) {
+    if (!ChessPiece.ColoredPiece.is(square.piece)) continue;
+    // only check player in check
+    if (square.piece.coloredPiece.color !== gameState.activeColor) continue;
+    const moves = PieceMoveMap[square.piece.coloredPiece.pieceType](square.piece.coloredPiece.color);
+    for (const move of moves) {
+      const validMoves = move.test(gameState, square.pos);
+      for (const validMove of validMoves) {
+        const gameStateCopy = deserialize(serialize(gameState));
+        validMove.exec(gameStateCopy);
+        if (isCheck(gameStateCopy, gameState.activeColor)) continue;
+        return false;
+      }
+    }
+  }
+  return true;
+}
---
+++ src/state/utils/tests/CheckUtils.test.ts
@@ -0,0 +1,69 @@
+import {
+  describe,
+  expect,
+  it,
+} from '@jest/globals';
+import { deserialize } from '../../../fen/deserializer';
+import {
+  FENString,
+  StandardStartPositionFEN,
+} from '../../../fen/FENString';
+import {
+  isCheck,
+  isCheckMate,
+} from '../CheckUtils';
+
+describe('CheckUtils', () => {
+  it('should report isCheck correctly', () => {
+    // Starting POS
+    expect(isCheck(deserialize(StandardStartPositionFEN))).toBe(false);
+
+    // Check: Black's king on e8, White's rook on e1, and it's Black's move.
+    expect(isCheck(deserialize('4k3/8/8/8/8/8/8/4R3 b - - 0 1' as FENString))).toBe(true);
+
+    // Check: White's king on e1, Black's rook on e8, and it's White's move.
+    expect(isCheck(deserialize('4r3/8/8/8/8/8/8/4K3 w - - 0 1' as FENString))).toBe(true);
+
+   // Not Checkmate (but still in check): White's king on e1, Black's rook on e8, Black's bishop on b5, and it's White's move.
+    expect(isCheck(deserialize('4r3/8/8/1b6/8/8/8/4K3 w - - 0 1' as FENString))).toBe(true);
+
+    // Not Checkmate and not in check: White's king on e1, Black's rook on e7, and it's White's move.
+    expect(isCheck(deserialize('8/4r3/8/8/8/8/8/4K3 w - - 0 1' as FENString))).toBe(true);
+
+   // Check: Black's king on e8, White's bishop on a4, and it's Black's move.
+    expect(isCheck(deserialize('4k3/8/8/8/B7/8/8/8 b - - 0 1' as FENString))).toBe(true);
+
+    // Not Checkmate (but still in check): Black's king on h8, White's queen on h5, and it's Black's move.
+    expect(isCheck(deserialize('7k/8/8/7Q/8/8/8/8 b - - 0 1' as FENString))).toBe(true);
+
+    // Not Checkmate and not in check: Black's king on h8, White's queen on g4, and it's Black's move.
+    expect(isCheck(deserialize('7k/8/8/8/6Q1/8/8/8 b - - 0 1' as FENString))).toBe(false);
+
+    // Check: White's king on e1, Black's knight on d3, and it's White's move.
+    expect(isCheck(deserialize('8/8/8/8/8/3n4/8/4K3 w - - 0 1' as FENString))).toBe(true);
+
+    // Not Checkmate and not in check: White's king on e1, Black's knight on c3, and it's White's move.
+    expect(isCheck(deserialize('8/8/8/8/8/2n5/8/4K3 w - - 0 1' as FENString))).toBe(false);
+
+
+  });
+  it('should report isCheckMate correctly', () => {
+    // Starting POS
+    expect(isCheckMate(deserialize(StandardStartPositionFEN))).toBe(false);
+
+    // Not Checkmate (but still in check): White's king on e1, Black's rook on e8, Black's bishop on b5, and it's White's move.
+    expect(isCheckMate(deserialize('4r3/8/8/1b6/8/8/8/4K3 w - - 0 1' as FENString))).toBe(false);
+
+    // Not Checkmate (but still in check): Black's king on h8, White's queen on h5, and it's Black's move.
+    expect(isCheckMate(deserialize('7k/8/8/7Q/8/8/8/8 b - - 0 1' as FENString))).toBe(false);
+
+    // Checkmate: Black's king on e8, White's rooks on a8 and a7, and it's Black's move.
+    expect(isCheckMate(deserialize('R3k3/R7/8/8/8/8/8/8 b - - 0 1' as FENString))).toBe(true);
+
+    // Not Checkmate: Black's king on h8, White's queen on h6, and it's Black's move.
+    expect(isCheckMate(deserialize('7k/8/7Q/8/8/8/8/8 b - - 0 1' as FENString))).toBe(false);
+
+    // Checkmate: White's king on e1, Black's rooks on d8 and f8, and it's White's move.
+    expect(isCheckMate(deserialize('3rr3/8/8/8/8/8/8/4K3 w - - 0 1' as FENString))).toBe(false);
+  });
+});
