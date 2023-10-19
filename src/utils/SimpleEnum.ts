6931de418b961eaf23318f383dde378a43bbf741
bstriker <strikeforcezero@gmail.com>
2023-10-19 02:14:44 -0400

    refactor(utils): simplify simple enum typings to allow easier refactoring in ide

--- src/utils/SimpleEnum.ts
+++ src/utils/SimpleEnum.ts
@@ -4,7 +4,6 @@ import {
 } from 'type-fest';
 import { deepFreeze } from './deepFreeze';
 
-export type SimpleEnum<T extends Record<K, V>, K extends PropertyKey, V> = T;
 export type SimpleEnumTypeOf<T> = ValueOf<T>;
 
 /**
@@ -25,7 +24,7 @@ export type SimpleEnumTypeOf<T> = ValueOf<T>;
  * @param {T} obj - The object to be returned as an enum.
  * @returns {T} - The input object returned as is.
  */
-export function asSimpleEnum<T extends Record<K, V>, K extends PropertyKey, V>(obj: T): ReadonlyDeep<SimpleEnum<T, K, V>> {
+export function asSimpleEnum<T>(obj: T): ReadonlyDeep<T> {
   return deepFreeze(obj);
 }
 
@@ -48,7 +47,7 @@ export function asSimpleEnum<T extends Record<K, V>, K extends PropertyKey, V>(o
  * @param {T} obj - The object to be returned as an enum.
  * @returns {T} - The input object returned as is, with the `const` modifier applied to the type parameter V.
  */
-export function asSimpleConstEnum<const T extends Record<K, V>, const K extends PropertyKey, const V>(obj: T): ReadonlyDeep<SimpleEnum<T, K, V>> {
+export function asSimpleConstEnum<const T>(obj: T): ReadonlyDeep<T> {
   return deepFreeze(obj);
 }
 
