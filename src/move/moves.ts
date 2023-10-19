484aeac6731e4b6a7916c0fcd5d5ff68505e4555
bstriker <strikeforcezero@gmail.com>
2023-10-18 22:06:53 -0400

    refactor(moves): add testExec() to ExecutableMove

--- src/move/moves.ts
+++ src/move/moves.ts
@@ -27,6 +27,7 @@ import {
   last,
   sum,
 } from '../utils/array';
+import { Result } from '../utils/Result';
 import { zipExact } from '../utils/zip';
 import {
   AnyDiagonalDirection,
@@ -123,6 +124,7 @@ export type ExecutableMove = {
   // TODO: not sure how i feel about passing in moveHandler
   //  but until we can find a better way to remove the circular reference its required
   exec(gameState: GameState, moveHandler: MoveHandler, updateGameStatus?: boolean): ChessPiece,
+  tryExec(...args: Parameters<ExecutableMove['exec']>): Result<ReturnType<ExecutableMove['exec']>, unknown>,
 }
 
 export function executableMove(
@@ -137,6 +139,13 @@ export function executableMove(
     expectedCapturePos,
     exec(gameState: GameState, moveHandler: MoveHandler, updateGameStatus = true) {
       return moveHandler(gameState, this.fromPos, this.toPos, this.expectedCapturePos, alternateMoveHandler, updateGameStatus);
+    },
+    tryExec(...args: Parameters<typeof this.exec>) {
+      try {
+        return Result.Ok(this.exec(...args));
+      } catch (err) {
+        return Result.Err(err);
+      }
     },
   };
 }
