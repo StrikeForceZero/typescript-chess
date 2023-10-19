82eb0b7d87c39b7154e590d3366cfb6c75fba05a
bstriker <strikeforcezero@gmail.com>
2023-10-18 23:56:34 -0400

    feat(utils): add omit method in object utils

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,4 +1,5 @@
 import { ValueOf } from 'type-fest';
+import { argsToArray } from './array';
 
 export function getClassName(value: unknown): string | null {
   if (value && typeof value === 'object' && value.constructor && value.constructor.name) {
@@ -9,4 +10,15 @@ export function getClassName(value: unknown): string | null {
 
 export function entries<T extends Record<string, unknown>>(obj: T): [keyof T, ValueOf<T>][] {
   return Object.entries(obj) as [keyof T, ValueOf<T>][];
+}
+
+export function omit<T, K extends keyof T>(obj: T, ...keys: readonly  [K, ...K[]]): Omit<T, K>;
+export function omit<T, K extends keyof T>(obj: T, keys: readonly K[]): Omit<T, K>;
+export function omit<T, K extends keyof T>(obj: T, ...keysOrArray: readonly K[] ): Omit<T, K> {
+  const keys = argsToArray(keysOrArray);
+  const result = { ...obj };
+  for(const key of keys) {
+    delete result[key];
+  }
+  return result;
 }
---
+++ src/utils/tests/object.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from '@jest/globals';
+import { omit } from '../object';
+
+describe('object', () => {
+  describe('omit', () => {
+    it('should omit', () => {
+      const obj = { foo: 1, bar: 2, fizz: 'buzz', buzz: 'fizz' };
+      const keys = ['fizz', 'buzz'] as const;
+      expect(omit(obj, keys)).toStrictEqual({ foo: 1, bar: 2 });
+      expect(omit(obj, 'fizz', 'buzz')).toStrictEqual({ foo: 1, bar: 2 });
+      expect(omit(obj, ['fizz', 'buzz'])).toStrictEqual({ foo: 1, bar: 2 });
+      expect(omit(obj, ...keys)).toStrictEqual({ foo: 1, bar: 2 });
+    });
+  });
+});
