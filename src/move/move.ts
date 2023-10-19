1ba686556f65a834da41a1c29d2032eb8257261f
bstriker <strikeforcezero@gmail.com>
2023-10-18 22:36:01 -0400

    feat(move): add user facing move handler

---
+++ src/move/move.ts
@@ -0,0 +1,59 @@
+import { BoardPosition } from '../board/BoardPosition';
+import { getChessPieceColoredOrThrow } from '../board/utils/BoardUtils';
+import { GameState } from '../state/GameState';
+import {
+  count,
+  isNotEmpty,
+  last,
+  tallyBy,
+} from '../utils/array';
+import { entries } from '../utils/object';
+import {
+  ExecutableMove,
+  MoveType,
+} from './moves';
+import { performMove } from './performMove';
+import { PieceMoveMap } from './PieceMoveMap';
+
+function isSameMoveFactory(fromPos: BoardPosition, toPos: BoardPosition) {
+  return (move: ExecutableMove) => {
+    return move.fromPos.toString() === fromPos.toString() && move.toPos.toString() === toPos.toString();
+  };
+}
+
+export function move(gameState: GameState, fromPos: BoardPosition, toPos: BoardPosition): void {
+  const movingPiece = getChessPieceColoredOrThrow(gameState.board, fromPos);
+  const moves = PieceMoveMap[movingPiece.coloredPiece.pieceType](movingPiece.coloredPiece.color).flat();
+  const matchingMoves = [];
+  for (const move of moves) {
+    const validMoves = move.test(gameState, fromPos);
+    if (!isNotEmpty(validMoves)) {
+      continue;
+    }
+    const isSameMove = isSameMoveFactory(fromPos, toPos);
+    const moveIndex = validMoves.findIndex(isSameMove);
+    if (moveIndex === -1) {
+      continue;
+    }
+    if (moveIndex !== validMoves.findLastIndex(isSameMove)) {
+      // TODO: shouldn't occur, but until we can prove it in a test, leave it here
+      throw new Error(`ambiguous move ${move.moveType}: ${fromPos} -> ${toPos}`);
+    }
+    matchingMoves.push({ move, moveIndex });
+  }
+  if (!isNotEmpty(matchingMoves)) {
+    throw new Error(`Invalid move! ${fromPos} -> ${toPos}`);
+  }
+  if (matchingMoves.length > 1) {
+    const matchingMoveByTypeMap = tallyBy(matchingMoves, value => value.move.moveType);
+    // TODO: maybe remove redundant LJump moves from PieceMoveMap
+    if (count(entries(matchingMoveByTypeMap), ([key, _value]) => key !== MoveType.LJump)) {
+      // TODO: shouldn't occur, but until we can prove it in a test, leave it here
+      const moveTypes = matchingMoves.map(({move}) => move.moveType);
+      throw new Error(`ambiguous move ${JSON.stringify(moveTypes)}: ${fromPos} -> ${toPos}`);
+    }
+  }
+  const matchingMove = last(matchingMoves);
+  // TODO: is it worth keeping process vs just calling chosenMove.exec(gameState, performMove) directly?
+  matchingMove.move.process(gameState, performMove, fromPos, matchingMove.moveIndex);
+}
---
+++ src/move/tests/move.test.ts
@@ -0,0 +1,58 @@
+import {
+  beforeEach,
+  describe,
+  expect,
+  it,
+} from '@jest/globals';
+import { BoardPosition } from '../../board/BoardPosition';
+import { deserialize } from '../../fen/deserializer';
+import { StandardStartPositionFEN } from '../../fen/FENString';
+import {
+  BlackKing,
+  BlackPawn,
+  ChessPieceColored,
+  WhiteKnight,
+  WhitePawn,
+  WhiteQueen,
+} from '../../piece/ChessPiece';
+import { GameStatus } from '../../state/GameStatus';
+import { move } from '../move';
+
+describe('move', () => {
+  let gameState = deserialize(StandardStartPositionFEN);
+  beforeEach(() => {
+    gameState = deserialize(StandardStartPositionFEN);
+  });
+  function moveAndValidate(fromPos: string, toPos: string, expectedPiece: ChessPieceColored) {
+    const from = BoardPosition.fromString(fromPos);
+    const to = BoardPosition.fromString(toPos);
+    move(gameState, from, to);
+    expect(gameState.board.getPieceFromPos(to)).toStrictEqual(expectedPiece);
+  }
+  it('should handle simple move', () => {
+    moveAndValidate('b1', 'c3', WhiteKnight);
+  });
+  // TODO: this is more of an e2e test
+  it('should handle wrong color move', () => {
+    moveAndValidate('e2', 'e4', WhitePawn);
+    // wrong color
+    expect(() => moveAndValidate('d1', 'h5', WhiteQueen)).toThrow();
+  });
+  it('should handle capture move', () => {
+    moveAndValidate('e2', 'e4', WhitePawn);
+    moveAndValidate('f7', 'f5', BlackPawn);
+    moveAndValidate('d1', 'g4', WhiteQueen);
+    moveAndValidate('f5', 'g4', BlackPawn);
+  });
+  it('should handle check', () => {
+    moveAndValidate('e2', 'e4', WhitePawn);
+    moveAndValidate('f7', 'f5', BlackPawn);
+    moveAndValidate('d1', 'h5', WhiteQueen);
+    expect(gameState.gameStatus).toBe(GameStatus.Check);
+    // should fail because this move does not get us out of check
+    expect(() => moveAndValidate('e8', 'f7', BlackKing)).toThrow();
+    // should pass because we are blocking the check
+    moveAndValidate('g7', 'g6', BlackPawn);
+    expect(gameState.gameStatus).toBe(GameStatus.InProgress);
+  });
+});
